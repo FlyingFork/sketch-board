27f711fe0d9bbf9cb9b0e3c702734a2ffe765425
FlyingFork <62627418+FlyingFork@users.noreply.github.com>
2025-11-15 03:39:02 +0200

    add: Subitems

--- scripts/menu.js
+++ scripts/menu.js
@@ -13,6 +13,7 @@ function showBootstrapModal(Title, Body) {
 
   DOMTitle.textContent = Title;
   DOMBody.innerHTML = Body;
+  DOMFotter.innerHTML = "";
 
   const CloseButton = document.createElement("button");
   CloseButton.type = "button";
@@ -39,8 +40,38 @@ function ModalListener() {
   const identifier = this.dataset.modalTitle;
 
   if (identifier == "Start fresh?") {
-    console.log("Clear Canvas");
+    const DOMCanvas = document.getElementById("board");
+    const CTX = DOMCanvas.getContext("2d");
+    CTX.clearRect(0, 0, canvas.width, canvas.height);
   }
+}
+
+function populateSubitems(items) {
+  const DOMRoot = document.getElementById("subitems");
+  DOMRoot.style.display = "flex";
+  DOMRoot.innerHTML = "";
+
+  items.forEach((element) => {
+    const DOMElement = document.createElement("div");
+
+    if (element.id) DOMElement.id = element.id;
+
+    if (element.inner) DOMElement.innerHTML = element.inner;
+
+    if (element.tooltip) {
+      DOMElement.dataset.bsPlacement = "right";
+      DOMElement.dataset.bsToggle = "tooltip";
+      DOMElement.dataset.bsTitle = element.tooltip;
+      new bootstrap.Tooltip(DOMElement);
+    }
+
+    DOMRoot.appendChild(DOMElement);
+
+    if (element.color) {
+      const DOMSvg = DOMElement.querySelector("svg");
+      if (DOMSvg) DOMSvg.style.fill = element.color;
+    }
+  });
 }
 
 function populateMenu(DOMRoot, JSONData) {
@@ -57,13 +88,31 @@ function populateMenu(DOMRoot, JSONData) {
       DOMElement.dataset.bsPlacement = "top";
       DOMElement.dataset.bsToggle = "tooltip";
       DOMElement.dataset.bsTitle = element.tooltip;
+      new bootstrap.Tooltip(DOMElement);
     }
 
-    if (element.modal) {
-      DOMElement.addEventListener("click", () => {
+    if (element.default) DOMElement.classList.add("selected");
+
+    DOMElement.addEventListener("click", () => {
+      if (element.selectable || element.subitems) {
+        let DOMOldSelected = DOMRoot.querySelector(".selected");
+        if (DOMOldSelected) {
+          DOMOldSelected.classList.remove("selected");
+
+          let DOMSubItems = document.getElementById("subitems");
+          DOMSubItems.style.display = "none";
+        }
+      }
+
+      if (element.modal) {
         showBootstrapModal(element.modal.title, element.modal.body);
-      });
-    }
+      } else {
+        if (element.selectable || element.subitems)
+          DOMElement.classList.add("selected");
+
+        if (element.subitems) populateSubitems(element.subitems);
+      }
+    });
 
     DOMRoot.appendChild(DOMElement);
   });
@@ -73,15 +122,5 @@ fetch("./data/menu-buttons.json")
   .then((response) => response.json())
   .then((data) => {
     const DOMToolbar = document.getElementById("toolbar");
-    const DOMZoombar = document.getElementById("zoombar");
     populateMenu(DOMToolbar, data.toolbar);
-    populateMenu(DOMZoombar, data.zoombar);
-
-    // Load Bootstrap Tooltips
-    const tooltipTriggerList = document.querySelectorAll(
-      '[data-bs-toggle="tooltip"]'
-    );
-    const tooltipList = [...tooltipTriggerList].map(
-      (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
-    );
   });
