c2b9d80a3a60c8849b661e0723504acde9fddb7d
FlyingFork <62627418+FlyingFork@users.noreply.github.com>
2025-11-16 03:00:26 +0200

    add: Store selected items and colors

---
+++ scripts/canvas.js
@@ -0,0 +1,3 @@
+export function test() {
+  console.log("Called from canvas");
+}
--- scripts/menu.js
+++ scripts/menu.js
@@ -1,3 +1,9 @@
+import { test } from "./canvas.js";
+
+let CURRENT_OPTION = "pointer";
+let CURRENT_COLOR = "#000000";
+let COLORS = {};
+
 function showBootstrapModal(Title, Body) {
   const bootstrapModal = document.getElementById("bootstrapModal");
 
@@ -42,16 +48,16 @@ function ModalListener() {
   if (identifier == "Start fresh?") {
     const DOMCanvas = document.getElementById("board");
     const CTX = DOMCanvas.getContext("2d");
-    CTX.clearRect(0, 0, canvas.width, canvas.height);
+    CTX.clearRect(0, 0, DOMCanvas.width, DOMCanvas.height);
   }
 }
 
-function populateSubitems(items) {
+function populateSubitems(items, isColors = false) {
   const DOMRoot = document.getElementById("subitems");
   DOMRoot.style.display = "flex";
   DOMRoot.innerHTML = "";
 
-  items.forEach((element) => {
+  let addSubItem = (element) => {
     const DOMElement = document.createElement("div");
 
     if (element.id) DOMElement.id = element.id;
@@ -70,8 +76,47 @@ function populateSubitems(items) {
     if (element.color) {
       const DOMSvg = DOMElement.querySelector("svg");
       if (DOMSvg) DOMSvg.style.fill = element.color;
+      if (CURRENT_COLOR === element.color) DOMElement.classList.add("selected");
     }
-  });
+
+    DOMElement.addEventListener("click", () => {
+      let DOMOldSelected = DOMRoot.querySelector(".selected");
+      if (DOMOldSelected) DOMOldSelected.classList.remove("selected");
+
+      DOMElement.classList.add("selected");
+
+      if (element.color) {
+        CURRENT_COLOR = element.color;
+      }
+    });
+  };
+
+  if (isColors) {
+    COLORS.forEach(addSubItem);
+
+    const DOMSeparator = document.createElement("div");
+    DOMSeparator.classList.add("separator");
+    DOMSeparator.style = "width: 100%; height: 1px;";
+    DOMRoot.appendChild(DOMSeparator);
+
+    const DOMAdd = document.createElement("div");
+    DOMAdd.dataset.bsPlacement = "right";
+    DOMAdd.dataset.bsToggle = "tooltip";
+    DOMAdd.dataset.bsTitle = "Add Color";
+    new bootstrap.Tooltip(DOMAdd);
+    DOMAdd.innerHTML =
+      '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M352 128C352 110.3 337.7 96 320 96C302.3 96 288 110.3 288 128L288 288L128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352L288 352L288 512C288 529.7 302.3 544 320 544C337.7 544 352 529.7 352 512L352 352L512 352C529.7 352 544 337.7 544 320C544 302.3 529.7 288 512 288L352 288L352 128z"/></svg>';
+    DOMRoot.appendChild(DOMAdd);
+
+    // DOMAdd.addEventListener("click", () => {
+    //   COLORS.push({
+    //     "selectable": true,
+    //     "color": "#000000",
+    //   })
+    // })
+  } else {
+    items.forEach(addSubItem);
+  }
 }
 
 function populateMenu(DOMRoot, JSONData) {
@@ -91,7 +136,9 @@ function populateMenu(DOMRoot, JSONData) {
       new bootstrap.Tooltip(DOMElement);
     }
 
-    if (element.default) DOMElement.classList.add("selected");
+    if (CURRENT_OPTION === element.id) DOMElement.classList.add("selected");
+
+    if (element.deactivated) DOMElement.classList.add("deactivated");
 
     DOMElement.addEventListener("click", () => {
       if (element.selectable || element.subitems) {
@@ -107,10 +154,16 @@ function populateMenu(DOMRoot, JSONData) {
       if (element.modal) {
         showBootstrapModal(element.modal.title, element.modal.body);
       } else {
-        if (element.selectable || element.subitems)
+        if (element.selectable || element.subitems) {
           DOMElement.classList.add("selected");
+          CURRENT_OPTION = DOMElement.id;
+        }
 
-        if (element.subitems) populateSubitems(element.subitems);
+        if (element.subitems)
+          populateSubitems(
+            element.subitems,
+            (element.id === "palette" && true) || false
+          );
       }
     });
 
@@ -121,6 +174,8 @@ function populateMenu(DOMRoot, JSONData) {
 fetch("./data/menu-buttons.json")
   .then((response) => response.json())
   .then((data) => {
+    COLORS = data.toolbar[2].subitems;
     const DOMToolbar = document.getElementById("toolbar");
     populateMenu(DOMToolbar, data.toolbar);
+    test();
   });
