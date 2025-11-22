908b9caf84d04a75153d76f7aac758c7cf47515c
FlyingFork <62627418+FlyingFork@users.noreply.github.com>
2025-11-22 18:41:31 +0200

    refactor: Menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,9 +1,13 @@
 import { updateState, clearCanvas, addHistory, undo, redo } from "./canvas.js";
 
+const MIN_STROKE = 3;
+const MAX_STROKE = 30;
+
 let CURRENT_OPTION = "pointer";
 let CURRENT_COLOR = "#000000";
 let CURRENT_VOLUME = 0.6;
 let CURRENT_STROKE = 3;
+let STROKES = {};
 let COLORS = {};
 
 function addMenuItem(parent, data, tooltipPlacement = "right") {
@@ -42,6 +46,12 @@ function addMenuItem(parent, data, tooltipPlacement = "right") {
         'px ;"></div>';
 
       if (CURRENT_STROKE === data.size) DOMElement.classList.add("selected");
+
+      DOMElement.dataset.bsToggle = "tooltip";
+      DOMElement.dataset.bsTitle = "Stroke " + data.size;
+      DOMElement.dataset.bsPlacement = tooltipPlacement;
+
+      new bootstrap.Tooltip(DOMElement);
     } else if (data.id === "sound") {
       DOMElement.innerHTML = data.states[CURRENT_VOLUME];
     }
@@ -57,23 +67,30 @@ function addMenuItem(parent, data, tooltipPlacement = "right") {
       if (data.id === "redo") return redo();
     }
 
-    if (parent.id === "toolbar") {
+    if (parent.id === "toolbar" && !data.action) {
       let DOMSubitems = document.getElementById("subitems");
       DOMSubitems.style.display = "none";
     }
 
     if (!data.action) {
-      let DOMOldSelected = parent.querySelector(".selected");
-      if (DOMOldSelected) DOMOldSelected.classList.remove("selected");
+      let DOMOldSelected = parent.querySelectorAll(".selected");
+      if (DOMOldSelected)
+        DOMOldSelected.forEach((e) => {
+          if (data.subitems) {
+            if (e.id !== CURRENT_OPTION) e.classList.remove("selected");
+          } else {
+            e.classList.remove("selected");
+          }
+        });
 
       DOMElement.classList.add("selected");
     }
 
     if (data.subitems)
-      return populateSubitems(
-        data.subitems,
-        (data.id === "palette" && true) || false
-      );
+      return populateSubitems(data.subitems, {
+        isColors: (data.id === "palette" && true) || false,
+        isStrokes: (data.id === "stroke" && true) || false,
+      });
 
     if (data.size) {
       CURRENT_STROKE = data.size;
@@ -155,100 +172,135 @@ function ModalListener() {
   }
 }
 
-function populateSubitems(items, isColors = false) {
+function populateSubitems(
+  items,
+  options = { isColors: false, isStrokes: false }
+) {
   const DOMRoot = document.getElementById("subitems");
   DOMRoot.style.display = "flex";
   DOMRoot.innerHTML = "";
 
-  let subItems = (isColors && COLORS) || items;
+  let subItems = items;
+
+  if (options.isColors) {
+    subItems = COLORS;
+  } else if (options.isStrokes) {
+    subItems = STROKES;
+  }
+
   subItems.forEach((entry) => {
     addMenuItem(DOMRoot, entry, "right");
   });
 
-  if (isColors) {
+  if (options.isColors || options.isStrokes) {
     const DOMSeparator = document.createElement("div");
     DOMSeparator.classList.add("separator");
     DOMSeparator.style = "width: 100%; height: 1px;";
     DOMRoot.appendChild(DOMSeparator);
 
-    if (COLORS.length < 5) {
-      const DOMAddColor = document.createElement("div");
-      DOMAddColor.dataset.bsPlacement = "right";
-      DOMAddColor.dataset.bsToggle = "tooltip";
-      DOMAddColor.dataset.bsTitle = "Add Color";
-      DOMAddColor.innerHTML =
+    if (
+      (options.isColors && COLORS.length < 5) ||
+      (options.isStrokes && STROKES.length < 5)
+    ) {
+      const DOMAdd = document.createElement("div");
+      DOMAdd.dataset.bsPlacement = "right";
+      DOMAdd.dataset.bsToggle = "tooltip";
+      DOMAdd.dataset.bsTitle =
+        "Add " + ((options.isColors && "Color") || "Stroke");
+      DOMAdd.innerHTML =
         '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M352 128C352 110.3 337.7 96 320 96C302.3 96 288 110.3 288 128L288 288L128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352L288 352L288 512C288 529.7 302.3 544 320 544C337.7 544 352 529.7 352 512L352 352L512 352C529.7 352 544 337.7 544 320C544 302.3 529.7 288 512 288L352 288L352 128z"/></svg>';
 
-      new bootstrap.Tooltip(DOMAddColor);
-      DOMRoot.appendChild(DOMAddColor);
+      new bootstrap.Tooltip(DOMAdd);
+      DOMRoot.appendChild(DOMAdd);
 
-      DOMAddColor.addEventListener("click", () => {
-        COLORS.push({
-          color: "#000001",
-          editable: true,
-        });
+      DOMAdd.addEventListener("click", () => {
+        if (options.isColors) {
+          COLORS.push({
+            color: "#000001",
+          });
+        } else if (options.isStrokes) {
+          STROKES.push({
+            id: "stroke",
+            tooltip: "Stroke " + CURRENT_STROKE + 4,
+            size: CURRENT_STROKE + 4,
+          });
+        }
 
-        populateSubitems(items, isColors);
+        document.body.querySelectorAll(".tooltip").forEach((e) => e.remove());
+        populateSubitems(items, options);
       });
     }
 
-    const DOMEditColor = document.createElement("div");
-    DOMEditColor.dataset.bsPlacement = "right";
-    DOMEditColor.dataset.bsToggle = "tooltip";
-    DOMEditColor.dataset.bsTitle = "Edit Selected Color";
-    DOMEditColor.innerHTML =
+    const DOMEdit = document.createElement("div");
+    DOMEdit.innerHTML =
       '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.1.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M416.9 85.2L372 130.1L509.9 268L554.8 223.1C568.4 209.6 576 191.2 576 172C576 152.8 568.4 134.4 554.8 120.9L519.1 85.2C505.6 71.6 487.2 64 468 64C448.8 64 430.4 71.6 416.9 85.2zM338.1 164L122.9 379.1C112.2 389.8 104.4 403.2 100.3 417.8L64.9 545.6C62.6 553.9 64.9 562.9 71.1 569C77.3 575.1 86.2 577.5 94.5 575.2L222.3 539.7C236.9 535.6 250.2 527.9 261 517.1L476 301.9L338.1 164z"/></svg>';
-    new bootstrap.Tooltip(DOMEditColor);
 
-    const DOMColorSelector = document.createElement("input");
+    // DOMEdit.dataset.bsPlacement = "right";
+    // DOMEdit.dataset.bsToggle = "tooltip";
+    // DOMEdit.dataset.bsTitle = "Edit Selected";
+    // new bootstrap.Tooltip(DOMEdit);
 
-    DOMColorSelector.type = "color";
-    DOMColorSelector.id = "colorPicker";
+    if (!options.isStrokes) DOMRoot.appendChild(DOMEdit);
 
-    DOMColorSelector.value = CURRENT_COLOR;
+    if (options.isColors) {
+      const DOMColorSelector = document.createElement("input");
 
-    DOMColorSelector.style.width = 0;
-    DOMColorSelector.style.height = 0;
-    DOMColorSelector.style.opacity = 0;
-    DOMColorSelector.style.top = "50%";
-    DOMColorSelector.style.left = "50px";
-    DOMColorSelector.style.position = "absolute";
-    DOMColorSelector.style.pointerEvents = "none";
-    DOMColorSelector.style.transform = "translateY(-50%)";
+      DOMColorSelector.type = "color";
+      DOMColorSelector.id = "colorPicker";
 
-    DOMEditColor.addEventListener("click", () => {
-      DOMColorSelector.click();
       DOMColorSelector.value = CURRENT_COLOR;
-    });
 
-    DOMColorSelector.addEventListener("change", () => {
-      let NEW_COLOR = DOMColorSelector.value;
-      let OLD_COLOR = CURRENT_COLOR;
+      DOMColorSelector.style.width = 0;
+      DOMColorSelector.style.height = 0;
+      DOMColorSelector.style.opacity = 0;
+      DOMColorSelector.style.top = "50%";
+      DOMColorSelector.style.left = "50px";
+      DOMColorSelector.style.position = "absolute";
+      DOMColorSelector.style.pointerEvents = "none";
+      DOMColorSelector.style.transform = "translateY(-50%)";
 
-      CURRENT_COLOR = NEW_COLOR;
-      updateState("color", CURRENT_COLOR);
+      DOMEdit.addEventListener("click", () => {
+        DOMColorSelector.click();
+        DOMColorSelector.value = CURRENT_COLOR;
+      });
 
-      for (let index = 0; index < COLORS.length; index++) {
-        const entry = COLORS[index];
-        if (entry.color === OLD_COLOR) {
-          COLORS[index].color = CURRENT_COLOR;
-          let colorDIV = DOMRoot.querySelectorAll("#subitems > div")[index];
-          if (colorDIV) {
-            let circleDIV = colorDIV.querySelector("div");
-            if (circleDIV) {
-              circleDIV.style.backgroundColor = CURRENT_COLOR;
+      DOMColorSelector.addEventListener("change", () => {
+        let NEW_COLOR = DOMColorSelector.value;
+        let OLD_COLOR = CURRENT_COLOR;
+
+        CURRENT_COLOR = NEW_COLOR;
+        updateState("color", CURRENT_COLOR);
+
+        for (let index = 0; index < COLORS.length; index++) {
+          const entry = COLORS[index];
+          if (entry.color === OLD_COLOR) {
+            COLORS[index].color = CURRENT_COLOR;
+            let colorDIV = DOMRoot.querySelectorAll("#subitems > div")[index];
+            if (colorDIV) {
+              let circleDIV = colorDIV.querySelector("div");
+              if (circleDIV) {
+                circleDIV.style.backgroundColor = CURRENT_COLOR;
+              }
+            } else {
+              document.body
+                .querySelectorAll(".tooltip")
+                .forEach((e) => e.remove());
+              populateSubitems(items, options);
             }
-          } else {
-            populateSubitems(items, isColors);
+
+            break;
           }
-
-          break;
         }
-      }
-    });
+      });
 
-    DOMRoot.appendChild(DOMColorSelector);
-    DOMRoot.appendChild(DOMEditColor);
+      DOMRoot.appendChild(DOMColorSelector);
+    } else if (options.isStrokes) {
+      // let popover = new bootstrap.Popover(DOMEdit, {
+      //   content: "Test",
+      //   title: "Select Stroke Size",
+      //   trigger: "click"
+      // });
+    }
   }
 }
 
@@ -278,6 +330,7 @@ fetch("./data/menu-buttons.json")
   .then((response) => response.json())
   .then((data) => {
     COLORS = data.toolbar[3].subitems;
+    STROKES = data.toolbar[5].subitems;
     const DOMToolbar = document.getElementById("toolbar");
     populateMenu(DOMToolbar, data.toolbar);
     addHistory();
